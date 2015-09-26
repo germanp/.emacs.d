43b54fb9d300e916b2e64f1fc24e3ec9189ed751
gpodesta <gpodesta@notebook-german.lan>
2015-09-26 00:23:00 -0300

    Added inittial configuration files.

---
+++ init.el
@@ -0,0 +1,31 @@
+(load "~/.emacs.d/my-loadpackages.el")
+
+;; (add-hook 'after-init-hook '(lambda ()
+;;   (load "~/.emacs.d/my-noexternals.el")
+;;   ))
+
+
+;; Remove scrollbars, menu bars, and toolbars
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Wind-move
+(global-set-key (kbd "C-c C-j") 'windmove-left)
+(global-set-key (kbd "C-c C-k") 'windmove-down)
+(global-set-key (kbd "C-c C-l") 'windmove-up)
+(global-set-key (kbd "C-c C-;") 'windmove-right)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (deeper-blue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
